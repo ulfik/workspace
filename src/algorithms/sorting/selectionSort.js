2780a31feda8ddaf12f1d00e2184dc7810544060
ulfik <blazej.krystek@nokia.com>
2018-04-03 23:02:47 +0200

    selection sort

---
+++ src/algorithms/sorting/selectionSort.js
@@ -0,0 +1,25 @@
+ function selectionSort(array) {
+
+
+	return array;
+}
+
+function min(j, array){
+	var element = array[j];
+	for(var i=j; i<array.length; i++){
+		if(element > array[i]){
+			element = array[i];
+			indexOfMin = i;
+		}
+	}
+	return indexOfMin;
+}
+
+function swap(i, j, array){
+	var temp = array[i];
+	array[i] = array[j];
+	array[j] = temp;
+}
+
+module.exports = selectionSort;
+
---
+++ src/algorithms/sorting/selectionSort.spec.js
@@ -0,0 +1,33 @@
+var expect    = require("chai").expect;
+var selectionSort = require('./selectionSort');
+
+describe("ALGORITHMS", () => {
+	describe("Selection sort", () => {
+
+		it("should return not changed array when array is sorted", () => {
+	        // given
+	        const array = [1, 2, 3];
+		  	  // when 
+		  	 const result = selectionSort(array);
+		  	  // then 
+	        expect(result).to.deep.equal([1, 2, 3]);
+         });
+
+		it("should move first largest number to end of array", () => {
+	        // given
+	        const array = [4, 1, 2, 3];
+		  	  // when 
+		  	 const result = selectionSort(array);
+		  	  // then 
+	        expect(result).to.deep.equal([1, 2, 3, 4]);
+				 });
+		it("should sort array elements", () => {
+					// given
+					const array = [4, 3, 2, 1];
+					// when 
+					const result = selectionSort(array);
+					// then 
+					expect(result).to.deep.equal([1, 2, 3, 4]);
+         });
+    });
+});
